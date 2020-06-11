2a48f0b22947905d93f85f23d561b2c0111e49b5
Wajih-alzouhairy <alzouhairy-beamro@gmail.com>
2020-06-11 10:46:42 +0200

    my first React build

---
+++ june/11-06/src/js/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Text from "./text";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hi I am React</h1>
+      <Text />
+      <Text />
+      <Text />
+      <Text />
+    </div>
+  );
+};
+
+export default App;
