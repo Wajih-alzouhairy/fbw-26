ba1cfff1eab9bd14a731d7f4199859bb0e3ac2c6
Wajih-alzouhairy <alzouhairy-beamro@gmail.com>
2020-06-29 14:32:43 +0200

    update-list

---
+++ june/29-06/build/precache-manifest.15263134102f2b320a56c8a767dbd0ce.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "074208c7d650fda9464f83d1073deb50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e5495fdbc227d87107c",
+    "url": "/static/js/main.ea796989.chunk.js"
+  },
+  {
+    "revision": "521d6384da489d602379",
+    "url": "/static/js/runtime-main.5d52631a.js"
+  }
+]);
---
+++ june/29-06/build/static/js/main.ea796989.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp29-06"]=this["webpackJsonp29-06"]||[]).push([[0],[function(n,p,s){n.exports=s(1)},function(n,p){}],[[0,1]]]);
+//# sourceMappingURL=main.ea796989.chunk.js.map
