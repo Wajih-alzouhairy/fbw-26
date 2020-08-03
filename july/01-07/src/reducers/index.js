25c0178223576813708ac24c579df6f2659ea7cd
Wajih-alzouhairy <alzouhairy-beamro@gmail.com>
2020-08-03 10:51:57 +0200

    server

--- july/01-07/src/reducers/index.js
+++ july/01-07/src/reducers/index.js
@@ -1,18 +1,19 @@
-// step 2.
 const allReducers = (state = {}, action) => {
   const { cart, products } = state;
   const { product } = action;
   const { id, productName, price } = product || {};
-  let quantity, inventory;
+  let quantity, inventory, newCart;
+
   switch (action.type) {
     case "ADD_PRODUCT":
       quantity = cart[id] ? cart[id].quantity : 0;
       inventory =
         product.inventory > 0 ? product.inventory - 1 : product.inventory;
+
       return {
         ...state,
         products: {
-          ...product,
+          ...products,
           [id]: {
             ...product,
             inventory,
@@ -27,6 +28,46 @@ const allReducers = (state = {}, action) => {
             quantity: quantity + 1,
           },
         },
+      };
+    case "CHECKOUT":
+      return {
+        ...state,
+        cart: {},
+      };
+    case "REMOVE_ONE":
+      inventory = products[id].inventory;
+      quantity = product.quantity;
+      newCart = { ...cart };
+      delete newCart[id];
+      return {
+        ...state,
+        cart:
+          quantity > 1
+            ? { ...cart, [id]: { ...product, quantity: quantity - 1 } }
+            : newCart,
+        products: {
+          ...products,
+          [id]: {
+            ...product,
+            inventory: inventory + 1,
+          },
+        },
+      };
+    case "REMOVE_ALL":
+      inventory = products[id].inventory;
+      quantity = product.quantity;
+      newCart = { ...cart };
+      delete newCart[id];
+      return {
+        ...state,
+        products: {
+          ...products,
+          [id]: {
+            ...product,
+            inventory: inventory + quantity,
+          },
+        },
+        cart: newCart,
       };
     default:
       return state;
